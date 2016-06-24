0312d6a3c1e9f4085f6d2337c1e9f89cd110c498
Tiago Roldão <focus5.6@gmail.com>
2016-06-24 14:40:23 +0100

    updated demos

--- demos/first-copy/app/main.ts
+++
@@ -1,8 +0,0 @@
-
-// Import the core angular services.
-import { bootstrap } from "@angular/platform-browser-dynamic";
-
-// Import the application components and services.
-import { AppComponent } from "./app.component";
-
-bootstrap( AppComponent );
